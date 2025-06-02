d32486fe113d52c3512811bb9be7240bac2a5ca3
shruthi-reddy14 <amireddyshruthi14@gmail.com>
2025-06-02 11:21:22 +0530

    updated

--- src/components/App2.jsx
+++ src/components/App2.jsx
@@ -1,5 +1,5 @@
 import "./App2.css";
-import Product from "./Product";
+import product from "./product";
 export default function App2() {
   const products = [
     { id: 1, name: "Product 1", price: 23 },
